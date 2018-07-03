70611d9966db5b2b8c85126a4dc2947b100ca99a
Simon Bull <simon.bull.84@googlemail.com>
2018-07-03 09:50:19 +0100

    Scaffold package

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # login.dfe.public-api.jobs
+[![Build Status](https://travis-ci.org/DFE-Digital/login.dfe.public-api.jobs.svg?branch=master)](https://travis-ci.org/DFE-Digital/login.dfe.public-api.jobs)
+[![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest) [![jest](https://jestjs.io/img/jest-badge.svg)](https://github.com/facebook/jest)
+
 Jobs required for DfE Sign-in public api
---
+++ test/entry.test.js
@@ -0,0 +1,13 @@
+const { register } = require('./../lib');
+
+const config = {};
+const logger = {};
+
+describe('when registering jobs', () => {
+  it('then it should return invitation handlers', () => {
+    const handlers = register(config, logger);
+
+    expect(handlers.find(h => h.type === 'publicinvitationrequest_v1')).toBeDefined();
+    expect(handlers.find(h => h.type === 'publicinvitationcomplete_v1')).toBeDefined();
+  });
+});
